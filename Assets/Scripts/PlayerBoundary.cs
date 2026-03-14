290150547ae6174bc46c08837abbe64162001ab7
ashleyelw <ashleyelworthy@gmail.com>
2026-03-14 19:33:18 +1100

    Added boundary script
    Added a boundary script so the player and enemy submarines cannot swim up into the sky or beyond the scene

---
+++ Assets/Scripts/PlayerBoundary.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerBoundary : MonoBehaviour
+{
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
+    [SerializeField] private float minY;
+    [SerializeField] private float maxY;
+
+    void LateUpdate()
+    {
+        float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
+        float clampedY = Mathf.Clamp(transform.position.y, minY, maxY);
+
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+    }
+}
